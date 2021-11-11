c5288bd4299b0644644fb623eb242248461948da
Daris <daris.hodzic.dh@gmail.com>
2021-11-11 16:18:52 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
-
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 function App() {
-  return <div></div>;
+  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+  return (
+    <div>
+      <NewTodo />
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
