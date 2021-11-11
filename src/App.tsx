0e6f489cb851590db025c5136153a3e09b837468
Daris <daris.hodzic.dh@gmail.com>
2021-11-11 18:22:30 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,28 @@ import "./App.css";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 import NewTodo from "./components/NewTodo";
+import { useState } from "react";
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prev) => {
+      return prev.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodo) => {
+      return prevTodo.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div>
-      <NewTodo />
-      <Todos items={todos} />
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
